a5e3498d44ee92085bd6f55431f074e99d100a03
Philip R. Onffroy <datamaniac18@gmail.com>
2017-10-30 19:50:46 -0400

    Uploaded Chpt. 13 file

---
+++ PRO_13_VectorPractice.m
@@ -0,0 +1,44 @@
+% Section 13.2 
+% Example 7
+I = [25 211 818 642]
+J = [331 3227 1377 2570]
+A = I + J
+% Example 8
+p1 = 1
+p2 = 2
+p3 = 3
+p = [p1 p2 p3]
+C = 100*p % this is the answer in cents
+
+% Problem 24
+v = [73 80 91 65 84]
+w = [82 79 88 70 92]
+f = (2*w + v)/3
+%Math Symbolic toolbox
+%import math symbols
+%CAS Problem 59
+a = [x y z]
+b = [u v w]
+c = m*a + n*b
+A1 = dot(cross(a,b),c)
+A2 = cross(cross(a,b),cross(a,c))
+
+%CAS prob 60
+a = [x y z]
+b = [u v w]
+c = [r s t]
+
+A3 = dot(cross(b,c),a)
+A4 = dot(cross(a,b),2*a - b + c)
+
+%CAS 61 
+%a
+a = [1 2 3]
+b = [2 1 2]
+% for a dot c = 0, 
+c = [
+    
+%62 
+A = (
+
+
